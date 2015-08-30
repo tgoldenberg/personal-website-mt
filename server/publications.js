ae0e1b7160b9489a8d70ca344e2740351b826752
tgoldenberg <thomasagoldenberg@gmail.com>
2015-08-30 19:51:38 -0400

    configure app to pull latest Tweets from personal accounts

--- server/publications.js
+++ server/publications.js
@@ -1,3 +1,35 @@
 Meteor.publish('projects', function(options) {
   return Projects.find();
 });
+
+Twit = Meteor.npmRequire('twit'),
+Future = Meteor.npmRequire('fibers/future');
+
+var twit = new Twit({
+    consumer_key: process.env["TWITTER_CONSUMER_KEY"],
+    consumer_secret: process.env["TWITTER_CONSUMER_SECRET"],
+    access_token: process.env["TWITTER_ACCESS_TOKEN"],
+    access_token_secret: process.env["TWITTER_ACCESS_SECRET"]
+});
+
+
+Meteor.publish('tweets', function(query) {  
+  var self = this;
+  try {
+    twit.get('statuses/user_timeline', { q: 'user_id 2894299203', count: 3 }, function(response, data, err) {
+    	// console.log("data", data.map(function(tweet){return tweet.text}));
+    	var response = data.map(function(tweet){return [tweet.text]});
+    
+	    _.each(response, function(item) {
+	      console.log(item);
+
+	      self.added('tweets', Random.id(), item);
+	    });
+
+	    self.ready();
+
+    });
+  } catch(error) {
+    console.log(error);
+  }
+});
--- server/twitter.js
+++ server/twitter.js
@@ -2,22 +2,17 @@
 Twit = Meteor.npmRequire('twit'),
 Future = Meteor.npmRequire('fibers/future');
 
-function connectTwitter() {
+var twit = new Twit({
+    consumer_key: process.env["TWITTER_CONSUMER_KEY"],
+    consumer_secret: process.env["TWITTER_CONSUMER_SECRET"],
+    access_token: process.env["TWITTER_ACCESS_TOKEN"],
+    access_token_secret: process.env["TWITTER_ACCESS_SECRET"]
+});
 
-    var twit = new Twit({
-        consumer_key: process.env["TWITTER_CONSUMER_KEY"],
-        consumer_secret: process.env["TWITTER_CONSUMER_SECRET"],
-        access_token: process.env["TWITTER_ACCESS_TOKEN"],
-        access_token_secret: process.env["TWITTER_ACCESS_SECRET"]
-    });
 
-    twit.get('statuses/user_timeline', { q: 'user_id 2894299203', count: 100 }, function(err, data, response) {
-        console.log(data);
-    });
-}
-
-if (Meteor.isServer) {
-    Meteor.startup(function() {
-        connectTwitter();
+function getTweets() {
+    twit.get('statuses/user_timeline', { q: 'user_id 2894299203', count: 3 }, function(err, data, response) {
+    tweets = data.map(function(tweet) {return tweet.text;});
+    console.log(tweets);
     });
 }
