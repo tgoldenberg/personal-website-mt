eca52e185b2e77a6520c982c9840784a72eea569
tgoldenberg <thomasagoldenberg@gmail.com>
2015-09-01 11:19:57 -0400

    get Github repo feed and display in home page

--- client/templates/home/home.js
+++ client/templates/home/home.js
@@ -1,5 +1,8 @@
 Template.home.helpers({
 	tweets: function() {
 		return Tweets.find().map(function(tweet){return tweet[0]});
+	},
+	github: function() {
+		return Github.find({}, {sort: {updatedAt: -1}, limit: 3});
 	}
 })
--- server/publications.js
+++ server/publications.js
@@ -2,7 +2,7 @@ Meteor.publish('projects', function(options) {
   return Projects.find();
 });
 
-Twit = Meteor.npmRequire('twit'),
+Twit = Meteor.npmRequire('twit');
 Future = Meteor.npmRequire('fibers/future');
 
 var twit = new Twit({
@@ -12,17 +12,47 @@ var twit = new Twit({
     access_token_secret: process.env["TWITTER_ACCESS_SECRET"]
 });
 
+var github = new GitHub({
+    version: "3.0.0", // required
+    protocol: "https",
+    host: "api.github.com",
+    timeout: 5000,     // optional
+    headers: {
+        "user-agent": "My-Cool-GitHub-App" // GitHub is happy with a unique user agent
+    }
+});
+github.authenticate({
+    type: "basic",
+    username: "tgoldenberg",
+    password: process.env["GITHUB_PASSWORD"]
+});
 
-Meteor.publish('tweets', function(query) {  
+Meteor.publish('github', function(query) {
+  // console.log(github.repos);
   var self = this;
+  try {
+    github.repos.getFromUser({user: 'tgoldenberg'}, function(err, res) {
+    // console.log(res)
+      res.forEach(function(repo) {
+        self.added('github', Random.id(), {name: repo.name, updatedAt: repo.updated_at, url: repo.html_url, description: repo.description});
+      });
+	    self.ready();
+    });
+  } catch(error) {
+    console.log(error);
+  }
+})
+
+Meteor.publish('tweets', function(query) {
+  var self = this;
+  console.log("HI tweets");
   try {
     twit.get('statuses/user_timeline', { q: 'user_id 2894299203', count: 3 }, function(response, data, err) {
     	// console.log("data", data.map(function(tweet){return tweet.text}));
     	var response = data.map(function(tweet){return [tweet.text]});
-    
+
 	    _.each(response, function(item) {
 	      console.log(item);
-
 	      self.added('tweets', Random.id(), item);
 	    });
 
